15405d14f5ad766f70c7b7091f6977c5ed3d1d2e
Владислав Сидорик <uladzislau.sidoryk@hoster.by>
2024-08-29 03:25:45 +0300

    chore: Update site configuration URLs and links

--- .vitepress/theme/config/site.ts
+++ .vitepress/theme/config/site.ts
@@ -1,19 +1,19 @@
 export const siteConfig = {
     name: 'shadcn extended',
-    url: 'https://shadcn-vue.com',
-    ogImage: 'https://shadcn-vue.com/og.png',
+    url: 'https://shadcn-extended.vercel.app/',
+    ogImage: 'https://shadcn-extended.vercel.app/og.png',
     description: 'Extednded components for shadcn/vue',
     links: {
         twitter: 'https://twitter.com/huntabyte',
         github: 'https://github.com/huntabyte/shadcn-vue',
         shadTwitter: 'https://twitter.com/shadcn',
-        shadGithub: 'https://github.com/shadcn/ui',
+        shadGithub: 'https://github.com/SidVerson/shadcn-extended.git',
     },
     keywords: 'shadcn,Vue,Nuxt,Vue Components,TailwindCSS,Radix Vue',
 }
 
 export const announcementConfig = {
     icon: '✨',
-    title: 'Extended: Auto Form, Charts',
-    link: '/docs/components/auto-form.html',
+    title: 'Extended: Now available!',
+    link: '/docs/components/introduction.html',
 }
